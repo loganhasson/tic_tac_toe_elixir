39bc0c99d7668230440a9687b405983b4d655abf
Logan Hasson <logan.hasson@gmail.com>
2015-10-28 19:52:28 -0400

    Meh, not working yet

--- lib/tic_tac_toe/board.ex
+++ lib/tic_tac_toe/board.ex
@@ -1,6 +1,11 @@
 defmodule TicTacToe.Board do
   use GenServer
 
+  @winning_combinations [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+    [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+  ]
+
   def start do
     initial_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     {:ok, board} = GenServer.start(__MODULE__, initial_board)
@@ -15,6 +20,10 @@ defmodule TicTacToe.Board do
     GenServer.call(board, {:move, position})
   end
 
+  def winner(board) do
+    GenServer.call(board, :winner)
+  end
+
   def handle_call(:board, _from, board) do
     {:reply, board, board}
   end
@@ -23,6 +32,10 @@ defmodule TicTacToe.Board do
     {status, board} = board |> _move(position)
 
     {:reply, {status, board}, board}
+  end
+
+  def handle_call(:winner, _from, board) do
+    {:reply, board |> _winner, board}
   end
 
   defp _move(board, position) do
@@ -40,14 +53,31 @@ defmodule TicTacToe.Board do
     {:error, board}
   end
 
+  defp _winner(board) do
+    combos = Enum.filter(@winning_combinations, fn (combo) -> is_winning_combo(board, combo) end)
+    IO.puts(combos)
+    #|> get_winner
+    # combos = for combo <- @winning_combinations, is_winning_combo(board, combo), into: [], do: combo
+    #combos |> get_winner
+  end
+
+  defp get_winner([]), do: nil
+  defp get_winner(winning_combos), do: List.first(winning_combos) |> List.first
+
+  defp is_winning_combo(board, combo) do
+    tokens = Enum.map(combo, &(Enum.at(board, &1)))
+    #for index <- combo, into: [], do: Enum.at(board, index)
+    tokens |> are_all_same_tokens
+  end
+
+  defp are_all_same_tokens(token_list) do
+    Enum.uniq(token_list) |> Enum.count === 1
+  end
+
   defp current_player(board) do
     Enum.count(board, &(&1 === " ")) |> convert_count_to_player
   end
 
-  defp convert_count_to_player(empty_count) when rem(empty_count, 2) == 0 do
-    "O"
-  end
-  defp convert_count_to_player(empty_count) when rem(empty_count, 2) != 0 do
-    "X"
-  end
+  defp convert_count_to_player(empty_count) when rem(empty_count, 2) == 0, do: "O"
+  defp convert_count_to_player(empty_count) when rem(empty_count, 2) != 0, do: "X"
 end
